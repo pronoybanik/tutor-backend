eef87ae18ed34d51abc3de43107847e71665f787
PRONOY <pronoybanik82@gmail.com>
2025-02-28 22:51:54 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api/v1', router);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'welcome to the server 6000',
+  });
+});
+
+// Global error handling
+
+export default app;
